d5a7781e7b26008198c7b4e0f3ea32ec2ba02375
arfanur04 <arfanur04@gmail.com>
2024-04-26 18:10:27 +0600

    56-8 Update Single Coffee Information

--- index.js
+++ index.js
@@ -3,6 +3,33 @@ const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
+
+// > need to remove this before deployment
+const os = require("os");
+// Function to get the local IP address
+function getLocalIP() {
+	const interfaces = os.networkInterfaces();
+	const addresses = [];
+
+	for (let k in interfaces) {
+		for (let k2 in interfaces[k]) {
+			const address = interfaces[k][k2];
+			if (address.family === "IPv4" && !address.internal) {
+				addresses.push(address.address);
+			}
+		}
+	}
+
+	// console.log("Local IP Address:", addresses);
+	return addresses[0];
+}
+// Call the function to get the local IP address
+// getLocalIP();
+
+const ip = process.env.IP || getLocalIP();
+// const ip = process.env.IP || "192.168.0.109";
+//
+
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -46,6 +73,26 @@ async function run() {
 			res.send(result);
 		});
 
+		app.put("/coffee/:id", async (req, res) => {
+			const id = req.params.id;
+			const updatedCoffee = req.body;
+			const filter = { _id: new ObjectId(id) };
+			const option = { upsert: true };
+			const update = {
+				$set: {
+					name: updatedCoffee.name,
+					quantity: updatedCoffee.quantity,
+					supplier: updatedCoffee.supplier,
+					taste: updatedCoffee.taste,
+					category: updatedCoffee.category,
+					details: updatedCoffee.details,
+					photo: updatedCoffee.photo,
+				},
+			};
+			const result = await coffeeCollection.updateOne(filter, update, option);
+			res.send(result);
+		});
+
 		app.delete("/coffee/:id", async (req, res) => {
 			const id = req.params.id;
 			const query = { _id: new ObjectId(id) };
@@ -69,6 +116,11 @@ app.get("/", (req, res) => {
 	res.send("coffee making server is running");
 });
 
-app.listen(port, () => {
-	console.log(`Coffee server is listening on ${port}`);
+// app.listen(port, () => {
+// 	console.log(`Coffee server is listening on ${port}`);
+// });
+
+// > need to remove this before deployment
+app.listen(port, ip, () => {
+	console.log(`server is listening on http://${ip}:${port}`);
 });
